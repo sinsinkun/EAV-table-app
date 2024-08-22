70c09830f125c1917b5663ed56836a9baee9fa71
Chang Xiao <chang.x1994@gmail.com>
2024-08-21 22:57:51 -0400

    Basic EAV method impl

--- src/main/java/org/access/Controller.java
+++ src/main/java/org/access/Controller.java
@@ -1,9 +1,12 @@
 package org.access;
 
 import org.database.EavInterface;
+import org.database.EavView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 @RestController
@@ -28,5 +31,14 @@ public class Controller {
     public String index() {
         if (eav == null) reconnectEav();
         return "Hello world from SpringBoot";
+    }
+
+    @RequestMapping(method=RequestMethod.GET, path="/all")
+    public List<EavView> getAll() {
+        if (eav == null) {
+            reconnectEav();
+            return null;
+        }
+        return eav.getEverything();
     }
 }
