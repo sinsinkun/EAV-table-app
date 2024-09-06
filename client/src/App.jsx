ecdd390ab978d663758274ef6b6649d8a9976eca
Chang Xiao <chang.x1994@gmail.com>
2024-09-05 21:51:57 -0400

    Adjust vertical fit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,20 @@
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './theme/App.css';
-import Layout from './lib/layout';
 import store from "./store";
 
 import HomePage from './pages/home';
 import FallbackPage from './pages/fallback';
 
-function Redirect({ url }) {
-  redirect(url);
-  return null;
-}
-
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <Layout>
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route path="/index.html" element={<Redirect url="/" />} />
-            <Route path="*" element={<FallbackPage />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="*" element={<FallbackPage />} />
+        </Routes>
       </Provider>
     </BrowserRouter>
   )
